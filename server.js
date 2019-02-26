dedae4fdb0ec344ed45583b66b89a07eef30aa41
JoGom <jogoram428@gmail.com>
2019-02-26 15:16:23 -0700

    added changes to server.js

--- server.js
+++ server.js
@@ -1,42 +1,19 @@
-const 
-        gameport        = process.env.PORT || 4004,
-        io              = require('socket.io'),
-        express         = require('express'),
-        UUID            = require('node-uuid'),
-        verbose         = false,
-        app             = express.createServer();
 
-/* Express server set up. */
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const http = require("http").createServer();
+const io = require("socket.io")(http);
 
-//The express server handles passing our content to the browser,
-//As well as routing users where they need to go. This example is bare bones
-//and will serve any file the user requests from the root of your web server (where you launch the script from)
-//so keep this in mind - this is not a production script but a development teaching tool.
+io.on("connection", (socket) => {
+    socket.emit("welcome", "hello and welcome to the socket.io Server");
+    console.log("new client is Connected");
+});
 
-        //Tell the server to listen for incoming connections
-    app.listen(gameport);
-
-        //Log something so we know that it succeeded.
-    console.log('\t :: Express :: Listening on port ' + gameport );
-
-        //By default, we forward the / path to index.html automatically.
-    app.get( '/', function( req, res ){ 
-        res.sendfile( __dirname + '/simplest.html' );
-    });
+http.listen(port, () => {
+    console.log("Server is listening on localhost: " + port);
+});
 
 
-        //This handler will listen for requests on /*, any file from the root of our server.
-        //See expressjs documentation for more info on routing.
 
-    app.get( '/*' , function( req, res, next ) {
 
-            //This is the current file they have requested
-        var file = req.params[0]; 
-
-            //For debugging, we can track what files are requested.
-        if(verbose) console.log('\t :: Express :: file requested : ' + file);
-
-            //Send the requesting client the file.
-        res.sendfile( __dirname + '/' + file );
-
-    }); //app.get *
