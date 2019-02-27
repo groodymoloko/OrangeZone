aeed729d8a2237773d66870923d4a548ff4e9da7
JoGom <jogoram428@gmail.com>
2019-02-27 15:40:43 -0700

    indicates when user joins and diconnects from the server

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 
 const express = require('express');
 const app = express();
-const PORT = 8080;
-const http = require("http").createServer();
-const io = require("socket.io")(http);
+// const PORT = 8080;
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
 
 // Sets up the Express app to handle data parsing
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
-// Static directory
-// app.use(express.static("public"));
-
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
+
+// Static directory
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/js", express.static(__dirname + "/public/js"));
+
+
 
 io.on("connection", (socket) => {
     socket.emit("welcome", "hello and welcome to the socket.io Server");
@@ -26,8 +30,8 @@ io.on("connection", (socket) => {
     });
 });
 
-http.listen(PORT, () => {
-    console.log("Server is listening on localhost: " + PORT);
+server.listen(8080, () => {
+    console.log("Server is listening on localhost: " + 8080);
 });
 
 
