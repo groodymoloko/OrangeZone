11ecb26597166b1e505dedd9c679c3e195719f5e
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-02-27 20:05:38 -0700

    working on characters model and controller

---
+++ controllers/orangezone-controller.js
@@ -0,0 +1,23 @@
+// REQUIRE MODELS
+let db = require('../models/');
+
+// Routes
+module.exports = function(app) {
+    // Get route for characters
+    app.get('/', function(req, res) {
+        db.character.findAll({}).then(function(result) {
+            let charObj = {
+                characters: result
+            };
+            res.render('index', charObj)
+        });
+    });
+
+
+
+
+
+
+
+
+}
