a22ac00dc2c7e698ce8e4fc06e80f656d617abc7
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-03-02 10:29:21 -0700

    updating character select css

--- controllers/orangezone-controller.js
+++ controllers/orangezone-controller.js
@@ -12,18 +12,4 @@ module.exports = function(app) {
             res.render('characters', charObj)
         });
     });
-<<<<<<< HEAD
-
-    
-
-
-
-
-
-
-
-
-}
-=======
 };
->>>>>>> 34710f4ff1b114c3dd3d14114d2e6ceb0514a40a
