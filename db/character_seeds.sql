127ba27561c24fd0510f3ab706d8f27f1184bbcd
mk566 <mk566@nau.edu>
2019-03-04 18:38:42 -0700

    new additions

--- db/character_seeds.sql
+++ db/character_seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("Megalodon", "./assets/images/megalodon.jpeg", "Loves swimming and sushi. Couldn't care less about your silly game.", 3, 6, 10);
-INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("Raptor", "./assets/images/raptor.jpeg", "Loves Chris Pratt. Hates smiling.", 5, 7, 4);
-INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("T-Rex", "./assets/images/t_rex.jpeg", "Kind of a big deal.", 8, 6, 2);
-INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("Pterodactyl", "./assets/images/pterodactyl.jpeg", "Soars above the rest.", 0, 8, 8);
-INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("Dilophosaurus", "./assets/images/dinosaur_dilophosaurus.jpeg", "Will never win Jurasic Park's 'The Bachelor'.", 3, 10, 5);
+INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("Megalodon", "./assets/images/megalodon.jpg", "Jaws is how he Netflix and chills.", 3, 6, 10);
+INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("Raptor", "./assets/images/raptor.jpg", "Loves Chris Pratt. Hates smiling.", 5, 7, 4);
+INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("T-Rex", "./assets/images/T-rex.png", "Kind of a big deal.", 8, 6, 2);
+INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("Allosaurus", "./assets/images/Allosaurus.png", "Opponents end up All-o-saur.", 0, 8, 8);
+INSERT INTO characters (character_name, image_link, character_description, speed, intelligence, luck) VALUES ("Stegosaurus", "./assets/images/stegosaurus.jpg", "Will never win Jurasic Park's 'The Bachelor'.", 3, 10, 5);
 
