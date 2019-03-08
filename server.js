41aef298958aa762d7da378fde222660c071ea66
Jorge G <jogoram428@gmail.com>
2019-03-08 14:01:07 -0700

    updated session store with sequelize

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 const app = express();
 const server = require("http").createServer(app);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/config/config.json')[env];
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/config/config.json')[env];
+const cookieParser = require("cookie-parser");
 const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
+const connect = require("connect");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const MySQLStore = require('express-mysql-session')(session);
 const passport = require("passport")
 const io = require("socket.io")(server);
 const passportSocketIo = require("passport.socketio");
-const cookieParser = require("cookie-parser");
+const datab = require("./models/index");
 
 
-console.log(config.username);
 // Sets up the Express app to handle data parsing
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
@@ -23,21 +25,39 @@ app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
 //used to determine if user is logged in
-const options = {
-    host: config.host,
-    port: config.port,
-    user: config.username,
-    password: config.password,
-    database: config.database
-};
-const sessionStore = new MySQLStore(options);
+// const options = {
+//     host: config.host,
+//     port: config.port,
+//     user: config.username,
+//     password: config.password,
+//     database: config.database
+// };
+// const sessionStore = new MySQLStore(options);
+// console.log(datab.Sequelize);
+// connect().use(connect.session({
+//     store: new SequelizeStore(db)
+//     , secret: 'itsasecret'
+// }));
+let myStore = new SequelizeStore({
+    db: datab.sequelize
+})
 app.use(session({
-	secret: 'itsasecret',
+    secret: 'itsasecret',
     resave: false,
-    store: sessionStore,
+    store: myStore,
     saveUninitialized: false,
+    proxy: true,
     // cookie: {secure: true}
-}));
+}))
+
+// myStore.sync();
+// app.use(session({
+// 	secret: 'itsasecret',
+//     resave: false,
+//     store: sessionStore,
+//     saveUninitialized: false,
+//     // cookie: {secure: true}
+// }));
 //set up passport.js
 app.use(cookieParser());
 app.use(passport.initialize());
@@ -51,7 +71,7 @@ io.use(passportSocketIo.authorize({
     cookieParser: cookieParser,       // the same middleware you registrer in express
     key:          'connect.sid',       // the name of the cookie where express/connect stores its session_id
     secret:       'itsasecret',    // the session_secret to parse the cookie
-    store:        sessionStore,        // we NEED to use a sessionstore. no memorystore please
+    store:        myStore,        // we NEED to use a sessionstore. no memorystore please
     // success:      onAuthorizeSuccess,  // *optional* callback on success - read more below
     // fail:         onAuthorizeFail,     // *optional* callback on fail/error - read more below
 }));
