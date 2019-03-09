056e652fae9b704f4b3ff65f7bcd1f76113cb274
Jorge G <jogoram428@gmail.com>
2019-03-08 19:08:22 -0700

    cleaned up commented code in server.js

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 const app = express();
 const server = require("http").createServer(app);
-// const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/config/config.json')[env];
-// const cookieParser = require("cookie-parser");
 const session = require('express-session');
 const connect = require("connect");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const MySQLStore = require('express-mysql-session')(session);
 const passport = require("passport")
 const io = require("socket.io")(server);
 const passportSocketIo = require("passport.socketio");
@@ -24,23 +20,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
-//used to determine if user is logged in
-// const options = {
-//     host: config.host,
-//     port: config.port,
-//     user: config.username,
-//     password: config.password,
-//     database: config.database
-// };
-// const sessionStore = new MySQLStore(options);
-// console.log(datab.Sequelize);
-// connect().use(connect.session({
-//     store: new SequelizeStore(db)
-//     , secret: 'itsasecret'
-// }));
+
 let myStore = new SequelizeStore({
     db: datab.sequelize
-})
+});
 app.use(session({
     secret: 'itsasecret',
     resave: false,
@@ -48,7 +31,7 @@ app.use(session({
     saveUninitialized: false,
     proxy: true,
     cookie: {secure: true}
-}))
+}));
 
 // myStore.sync();
 // app.use(session({
@@ -68,7 +51,6 @@ app.use(function(req, res, next) {
     next();
 });
 io.use(passportSocketIo.authorize({
-    // cookieParser: cookieParser,       // the same middleware you registrer in express
     key:          'connect.sid',       // the name of the cookie where express/connect stores its session_id
     secret:       'itsasecret',    // the session_secret to parse the cookie
     store:        myStore,        // we NEED to use a sessionstore. no memorystore please
@@ -81,9 +63,6 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// app.get("/", function(req, res) {
-//     res.sendFile(__dirname + "/public/index.html");
-// });
 require("./routes/html-routes")(app);
 require("./routes/login")(app);
 require("./controllers/orangezone-controller")(app);
